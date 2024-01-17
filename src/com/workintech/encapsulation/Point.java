a7aad2a57861915f067e158ffc51b2b192789750
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-17 23:32:48 +0300

    point class'i icin 2 degeri set edebilen bir constructor tanimlandi

--- src/com/workintech/encapsulation/Point.java
+++ src/com/workintech/encapsulation/Point.java
@@ -3,4 +3,9 @@ package com.workintech.encapsulation;
 public class Point {
     private int x;
     private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
