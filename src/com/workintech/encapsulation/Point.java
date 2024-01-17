0e25ed84e71928ff33f449da83a022dfe409dde6
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-17 23:52:48 +0300

    distance methodu tanimlandi ve 3 farklı overloading yapildi

--- src/com/workintech/encapsulation/Point.java
+++ src/com/workintech/encapsulation/Point.java
@@ -24,4 +24,20 @@ public class Point {
     public void setY(int y){
         this.y = y;
     }
+
+    public double distance(){
+        return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+    }
+
+    public double distance(Point p){
+        if(p == null){
+            System.out.println("p object cannot be null!");
+            return 0;
+        }
+        return distance(p.x, p.y);
+    }
+
+    public double distance(int a, int b){
+        return Math.sqrt(Math.pow(x - a, 2) + Math.pow(y - b, 2));
+    }
 }
