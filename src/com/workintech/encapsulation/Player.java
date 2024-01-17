b97c9bbae4d24c4421137e6b2ad8e4050e20a3d2
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-18 00:54:02 +0300

    healthRemaining, loseHealth ve restoreHealth isminde 3 adet method tanimlandi

--- src/com/workintech/encapsulation/Player.java
+++ src/com/workintech/encapsulation/Player.java
@@ -20,4 +20,31 @@ public class Player {
             this.healthPercentage = 100;
         }
     }
+
+    public int healthRemaining(){
+        return this.healthPercentage;
+    }
+
+    public void loseHealth(int damage){
+        int healthRemain = healthPercentage - damage;
+        if(healthRemain<=0){
+            System.out.println(name + " player has been knocked out of game");
+        }
+        healthPercentage = healthRemain;
+        checkHealth(healthPercentage);
+    }
+
+    public void restoreHealth(int healthPotion){
+        this.healthPercentage += healthPotion;
+        checkHealth(this.healthPercentage);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", healthPercentage=" + healthPercentage +
+                ", weapon=" + weapon +
+                '}';
+    }
 }
