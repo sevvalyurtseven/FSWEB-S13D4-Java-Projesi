073b457aa56b7d121a5107adb3a132746bfa0ce2
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-18 00:25:35 +0300

    constructor olusturuldu

--- src/com/workintech/encapsulation/Player.java
+++ src/com/workintech/encapsulation/Player.java
@@ -4,4 +4,10 @@ public class Player {
     private String name;
     private int healthPercentage;
     private Weapon weapon;
+
+    public Player(String name, int healthPercentage, Weapon weapon){
+        this.name = name;
+        this.healthPercentage = healthPercentage;
+        this.weapon = weapon;
+    }
 }
